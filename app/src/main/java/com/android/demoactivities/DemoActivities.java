73ed90f7e8ca11f7283df20c5856c6cada3ff8fb
18045945 <aungmoethet2000@gmail.com>
2020-04-23 14:45:32 +0800

    DemoActivities -> done

--- app/src/main/java/com/android/demoactivities/DemoActivities.java
+++ app/src/main/java/com/android/demoactivities/DemoActivities.java
@@ -1,6 +1,27 @@
 package com.android.demoactivities;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class DemoActivities extends AppCompatActivity {
+
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_activities);
+
+       textView = findViewById(R.id.textViewData);
+
+       Intent intent = getIntent();
+
+       String[] info = intent.getStringArrayExtra("info");
+
+       textView.setText("You are "+info[0]+", age "+info[1]);
+    }
 }
--- app/src/main/java/com/android/demoactivities/MainActivity.java
+++ app/src/main/java/com/android/demoactivities/MainActivity.java
@@ -2,13 +2,38 @@ package com.android.demoactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etName, etAge;
+    Button btnDone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etName = findViewById(R.id.editTextName);
+        etAge = findViewById(R.id.editTextAge);
+
+        btnDone = findViewById(R.id.buttonDone);
+
+        btnDone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] info = {etName.getText().toString(), etAge.getText().toString()};
+
+                Intent intent = new Intent(MainActivity.this, DemoActivities.class);
+
+                intent.putExtra("info", info);
+
+                startActivity(intent);
+            }
+        });
     }
 }
